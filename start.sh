49d3c979b700139975189e2d4d80d06a64d6ccb2
whonion <47848969+whonion@users.noreply.github.com>
2022-09-28 15:06:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-!/bin/bash
-sudo apt-get update  && sudo apt-get upgrade && sudo systemctl deamon reload
+#!/bin/bash
+sudo apt-get update && \
+sudo apt-get upgrade && \
+sudo systemctl daemon-reload
